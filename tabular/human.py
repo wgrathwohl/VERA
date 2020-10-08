ce38d28c637ad836d5dd204aa14dc55dad9ea489
Jacob Kelly <jacob.jin.kelly@gmail.com>
2020-10-07 21:19:06 -0400

    clean up

--- tabular/human.py
+++ tabular/human.py
@@ -1,3 +1,6 @@
+"""
+Data processing for HUMAN.
+"""
 
 import os
 from collections import Counter
@@ -32,20 +35,6 @@ class HUMAN(utils.Tabular):
         with open(os.path.join(path, "test", "y_test.txt"), 'r') as f:
             labels_test = np.array([int(l.strip()) - 1 for l in f])
 
-        # import matplotlib.pyplot as plt
-        # print(labels_train)
-        # plt.hist(labels_train)
-        # plt.show()
-        #
-        # plt.clf()
-        # #print(labels_test)
-        # plt.hist(labels_test)
-        # plt.show()
-        # 1/0
-
-        #labels_train = data_train[data_train.columns[0]].values
-        #labels_test = data_test[data_test.columns[0]].values
-
         self.label_names = {
             0: "WALKING",
             1: "WALKING_UPSTAIRS",
@@ -55,18 +44,8 @@ class HUMAN(utils.Tabular):
             5: "LAYING",
         }
 
-        #data_train = data_train.drop(data_train.columns[0], axis=1)
-        #data_test = data_test.drop(data_test.columns[0], axis=1)
-
-        # Because the data set is messed up!
-        #data_test = data_test.drop(data_test.columns[-1], axis=1)
-
         data_train = data_train.to_numpy()
-        print(data_train.shape)
         data_test = data_test.to_numpy()
-
-        #print(data_train.mean(0), data_train.std(0))
-
 
         i = 0
         # Remove any features that have too many re-occurring real values.
@@ -74,23 +53,16 @@ class HUMAN(utils.Tabular):
         for feature in data_train.T:
             c = Counter(feature)
             max_count = np.array([v for k, v in sorted(c.items())])[0]
-            #print(max_count)
             if max_count > 5:
                 features_to_remove.append(i)
             i += 1
 
-        print(data_train.shape)
         data_train = data_train[:, np.array([i for i in range(data_train.shape[1]) if i not in features_to_remove])]
-        print(data_train.shape)
         data_test = data_test[:, np.array([i for i in range(data_test.shape[1]) if i not in features_to_remove])]
-        print(data_train.shape, labels_train.shape)
-        print(data_train[0])
         return {"trn": (data_train, labels_train),
                 "tst": (data_test, labels_test),
                 "shuffle": True}
 
 
 if __name__ == "__main__":
-    x = HUMAN("../data/HUMAN")
-    counts = np.bincount(x.trn.y)
-    print(counts.max() / counts.sum())
+    HUMAN("../data/HUMAN")
